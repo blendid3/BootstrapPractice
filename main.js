8e2710c758e853f7b844e1ec04f7057c89d92a32
blendid3 <zhqblendid@gmail.com>
2021-03-21 11:50:29 -0700

    update the whole html(including tag test, home page writing and so on )

---
+++ main.js
@@ -0,0 +1,7 @@
+$(document).ready(function () {
+    $( "#Myjumbotron" ).load( "snippets/Myjumbotron.html" );
+    $( "#Footer" ).load( "snippets/footer.html" );
+    $( "#Nav" ).load( "snippets/Nav.html" );
+    $("#IndexPage").load("snippets/IndexPage.html");
+});
+
